92734a03fc95685ae9d778496c1f89330ddc44a5
Amanuel Detebo <amanueldetebo13@gmail.com>
2022-04-15 14:06:23 +0300

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,20 @@
 #include "main.h"
 /**
-*_isupper - function that verifies if a  character is uppercase or not
-*@c: tested character
-*Return: returns 1 if it is uppercase , 0 if not
-*/
+ * _isupper - checkes upper case
+ * @c: print int
+ * Return: give 1 or 0
+ */
 
 int _isupper(int c)
 {
-if ((c >= 'A') && (c <= 'Z'))
+if (c >= 'A' && c <= 'Z')
+{
 return (1);
+}
 
+else
+{
 return (0);
+}
+
 }
